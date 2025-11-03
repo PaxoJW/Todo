29bc488ca8c1c065dfed3e5d387a8d6efea36721
PaxoJW <184737673+PaxoJW@users.noreply.github.com>
2025-11-03 10:20:31 +0100

    Refactor TodoNote and project factory functions; update ScreenController to display project details

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
-import {TodoNote, project} from './todoClass.js';
 
 
-const defaultNote = TodoNote("default note");
-const defaultProject = project("default project");
+//factory function of a note
+const TodoNote = function(title) {
+    const description = "";
+    const dueDate = "";
+    const priority = "";
+    
+    return {title, description, dueDate, priority};
+}
 
-defaultProject.attachTodo(defaultNote);
+//factory function of a project
+function project(projName) {
+    const todos = [];
+    const getTodos = () => todos;
+    const attachTodo = (todoCard) => {
+        todos.push(todoCard);
+    }
+    return {projName, getTodos, attachTodo};
+}
+
+function createProject(projName) {
+    return project(projName);
+};
 
 
+function ScreenController() {
+    const defaultNote = TodoNote("default note");
+    const defaultProject = project("default project");
+
+    defaultProject.attachTodo(defaultNote);
+
+    const div1 = document.querySelector('.div1');
+    div1.textContent = `Project Name: ${defaultProject.projName};
+    Todos: ${defaultProject.getTodos().map(todo => todo.title).join(', ')}`;
+}
+
+ScreenController();
