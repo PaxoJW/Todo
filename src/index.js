bdff38e6cc200ce7044358456765e0eec4259373
PaxoJW <184737673+PaxoJW@users.noreply.github.com>
2025-11-11 17:03:06 +0100

    BUNDLED CORRECTLY
    Everything is bundled correctly and can be used by
    running npx webpack serve. date-fns module is correctly
    added and working. Removed the script tag from the html file.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
+import { format } from 'date-fns';
+
+format(new Date(1, 11, 2014), "dd-MM-yyyy"); //=> "1-11-2014"
+
+
 function todoController() {
     //factory function of a note
     const todoNoteFun = function(title = "title", description, dueDate, priority) {
         
+        //date manipulation can be done here
+        if (dueDate) {
+            const parsedDate = new Date(dueDate);
+            dueDate = format(parsedDate, "dd-MM-yyyy");
+        } else {
+            dueDate = "No due date";
+        }
+
         const todoCard = {title, description, dueDate, priority};
         
         //other functions of notes can go here
@@ -23,9 +36,9 @@ function todoController() {
         }
 
         const assignProject = (project) => {
-            if (!project || !project.attachTodo) {
-                const project = projectModule.project(`${project}`);
-            }
+            // if (!project || !project.attachTodo) {
+            //     const project = projectModule.project(`${project}`);
+            // }
             project.attachTodo(todo); //Defined below
         }
 
@@ -46,11 +59,15 @@ function projectController() {
     //factory function of a project
     function projectFun(projName = "project name") {
         const todos = [];
+        const project = {projName, todos};
+        
         const getTodos = () => todos;
+       
         const attachTodo = (todoCard) => {
             todos.push(todoCard);
         }
-        return {projName, getTodos, attachTodo};
+        
+        return {project, getTodos, attachTodo};
     }
 
     return {projectFun};
@@ -59,22 +76,23 @@ function projectController() {
 
 //controller module to manage the screen
 function ScreenController() {
-    const todoNoteFun = todoController();
-    const projectFun = projectController();
+    const tdNControl = todoController();
+    const pjControl = projectController();
     
-    const defaultProject = projectFun.projectFun("Default Project");
-    const defaultTodo = todoNoteFun.todoNoteFun("Default Todo", "Sample description", "2024-01-01", "Low");
+    const defaultProject = pjControl.projectFun("Default Project");
+    const defaultTodo = tdNControl.todoNoteFun("Default Todo", "Sample description", "2024-05-23", "Low");
 
     defaultProject.attachTodo(defaultTodo);
 
-    console.log("Default project name:", defaultProject.projName);
+    console.log("Default project name:", defaultProject.project.projName);
     console.log("Todos inside default project:", defaultProject.getTodos());
     console.log("Default Todo Details:", defaultTodo);
 
     //Let's return both controllers so we can use them outside
-    return { todoNoteFun, projectFun };
+    return { tdNControl, pjControl };
 
 }
 
-//Only one app controller is needed
-const app1 = ScreenController();
+// //Only one app controller is needed
+const app = ScreenController();
+window.app = app; //For debugging purposes  
